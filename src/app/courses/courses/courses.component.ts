4094558431faf27d0863e16612a1565e4f301c1b
jomas94 <jomariojsa@gmail.com>
2022-09-20 00:14:18 +0200

    error dialog

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+
+  courses$:Observable <Course[]> ;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService:CoursesService ) {
+
+    this.courses$ = this.coursesService.list()
+    .pipe(
+        catchError( error => {
+          return of([]);
+        })
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+}
